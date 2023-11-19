c716a1f39e39b2cc11f8c9c13cfdeed49f1aee4b
PedroMesquitaa <pedrozd45@gmail.com>
2023-11-19 18:39:05 -0300

    teste

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -6,6 +6,8 @@ namespace MeuApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            var idade = 25;
+
             var texto = "Testando";
             Console.WriteLine(texto);
         }
