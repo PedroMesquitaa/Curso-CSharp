915b27967a187c4c6c36b0bd65c05922549076c0
PedroMesquitaa <pedrozd45@gmail.com>
2024-02-25 13:57:06 -0300

    att

--- MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
+++ MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04211f303d23b40ed38ba62ee18516443bc3e7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
