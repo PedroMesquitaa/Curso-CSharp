40b2c256dd82a14e172b19947e89ec9659f4fef8
PedroMesquitaa <pedrozd45@gmail.com>
2024-03-02 16:23:49 -0300

    Treinando

---
+++ Atividades/Ex02_DiasVividos/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace DiasVividos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("..:: Calculadora de Dias vividos ::..");
+            Console.WriteLine("1 - Abrir calculadora");
+            Console.WriteLine("2 - Sair");
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine("Digite a opção:");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch(opcao)
+            {
+                case 1: Calculadora(); break;
+                case 2: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Calculadora()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Calculadora\n");
+            Console.WriteLine("Digite seu ano de nascimento:");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano atual:");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            int diasHoje = (anoAtual - anoNascimento) * 365;
+
+            Console.WriteLine("\n..:: Relatório ::..");
+            Console.WriteLine($"Quantidade de dias vividos: {diasHoje}\n");
+
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
--- Atividades/Ex02_DiasVividos/obj/Debug/net8.0/Ex02_DiasVividos.AssemblyInfo.cs
+++ Atividades/Ex02_DiasVividos/obj/Debug/net8.0/Ex02_DiasVividos.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ex01_MediaNotas")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ex02_DiasVividos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ex01_MediaNotas")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ex01_MediaNotas")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2106e72b82350a876b0fcfe4937de26d35982a")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ex02_DiasVividos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ex02_DiasVividos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
