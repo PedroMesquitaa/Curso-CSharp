10bf5e168612a83b33fc40bb859cccd4e0b35423
PedroMesquitaa <pedrozd45@gmail.com>
2024-03-03 18:37:45 -0300

    Começo do projeto

---
+++ StopWatch/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo => 10s = segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime !=  time)
+            {
+                Console.Clear();
+
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
