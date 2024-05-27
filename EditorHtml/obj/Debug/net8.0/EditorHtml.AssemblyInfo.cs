522769a017ed88f6a9407161dcbfb651fc9f581c
PedroMesquitaa <pedrozd45@gmail.com>
2024-05-27 15:52:25 -0300

    Atividade finalizado

--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+320cfc29cb10a192697f17093af2efda86b57d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964866c70dbc1fdc01ad6204a65d4c166cdb0a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
