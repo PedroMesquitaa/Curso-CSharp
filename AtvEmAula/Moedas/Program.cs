d5825f36bfdb86fc800f9711a387b369564205a0
PedroMesquitaa <pedrozd45@gmail.com>
2024-05-31 00:07:00 -0300

    Atividades formatação de Moedas

---
+++ AtvEmAula/Moedas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+
+            Console.WriteLine(Math.Ceiling(valor));
+        }
+    }
+}
