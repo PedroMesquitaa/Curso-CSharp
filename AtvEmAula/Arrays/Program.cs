6e40e410cf221d5556ee91854c7991c888efeb75
PedroMesquitaa <pedrozd45@gmail.com>
2024-05-31 00:53:07 -0300

    Atividade Arrays

---
+++ AtvEmAula/Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var funcionarios = new Funcionario[5];
+            funcionarios[0] = new Funcionario() { Id = 2579, Nome = "Pedro" };
+
+            foreach(var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+                Console.WriteLine(funcionario.Nome);
+            }
+        }
+    }
+
+    public struct Funcionario
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+    }
+}
